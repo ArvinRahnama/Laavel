b80c90820c6bee0b07bcabcac95405eadb67ddc2
Arvin Rahnama <arvin.rnm@gmail.com>
2021-07-25 15:44:00 +0430

    Hmmmmmm

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Models\Articles;
+use Illuminate\Support\Facades\Validator;
+
 Route::get('/', function () {
-    return view('welcome');
+    dd(Articles::All());
+    return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::prefix('admin')->group(function() {
+    Route::get('/articles/create' , function() {
+
+        return view('admin.articles.create');
+    });
+    Route::post('/articles/create',function() {
+
+        $validate_data = Validator::make(request()->all() , [
+            'title' => 'required|min:10|max:50',
+            'body' => 'required'
+        ])->validated();
+
+
+        Articles::create([
+            'title' => $validate_data['title'],
+            'slug' => $validate_data['title'],
+            'body' => $validate_data['body'],
+        ]);
+
+        return redirect('/admin/articles/create');
+    });
+    Route::get('/articles/{id}/edit' , function($id) {
+       $article = Articles::findOrFail($id);
+
+       return view('admin.articles.edit' , [
+           'article' => $article
+       ]);
+    });
+    Route::put('/articles/{id}/edit' , function($id) {
+        $validate_data = Validator::make(request()->all() , [
+            'title' => 'required|min:10|max:50',
+            'body' => 'required'
+        ])->validated();
+
+        $article = Articles::findOrFail($id);
+
+        $article->update($validate_data);
+
+        return back();
+    });
+});
+
