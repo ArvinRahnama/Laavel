b1cca60c8fd30a4fcce74edd367e1c16d932c283
Arvin Rahnama <arvin.rnm@gmail.com>
2021-08-01 10:12:09 +0430

    cookies

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\testmail;
+use http\Cookie;
 use Illuminate\Http\Request;
 use App\Models\Articles;
 use Illuminate\Support\Facades\Mail;
@@ -12,14 +13,16 @@ class HomeController extends Controller
     public function Home()
     {
 //        Mail::to('arvin.rnm@gmail.com')->send(new testmail('Arvin', 2001));
-        session(['key'=>'value']);
+//        session(['key'=>'value']);
+        cookie()->queue(Cookie('name',"hasan",1));
         $articles = Articles::all();
         return view('index', compact('articles'));
     }
 
     public function about()
     {
-        dd(session()->all());
+//        dd(session()->all());
+        dd(request()->cookie('name'));
         return view('about');
     }
 
